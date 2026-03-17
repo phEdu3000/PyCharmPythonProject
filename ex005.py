44319477cadf9ecc07ba4dc42e4b5be93269b37e
Raphael Eduardo Ferreira de Oliveira <eduardorafael275@gmail.com>
2026-03-17 11:10:26 -0300

    Dobro e triplo
    python logica

---
+++ ex005.py
@@ -0,0 +1,12 @@
+n1=int(input('digite a posiçao do numero'))
+
+ant=n1-1
+suc=n1+1
+
+print('Seu numero é {}\n Seu antecessor é {}\n Seu sucessor é {}'.format(n1,ant,suc));
+
+dob=n1*2
+trip=n1*3
+raiz=n1**(1/2)
+
+print('Seu dobro é {} \n seu triplo é {}\n sua raiz será {:.2f}'.format(dob,trip,raiz))
