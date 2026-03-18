351730aa88804ab2dcbc914d3eff3a1c05554a16
Raphael Eduardo Ferreira de Oliveira <eduardorafael275@gmail.com>
2026-03-18 13:02:58 -0300

    Conversor de Moedas.py

--- Conversor de Moedas.py
+++ Conversor de Moedas.py
@@ -7,7 +7,7 @@ kwanza=real/0.0057
 opcao= int(input('Escolha a moeda : \n1-Dolar\n2-Euro\n3-Kwanza\n'))
 match opcao:
     case 1:
-    print('Sua conversão de R${:.2f} em dolar deu ${:.2f}'.format(real,dolar))
+        print('Sua conversão de R${:.2f} em dolar deu ${:.2f}'.format(real,dolar))
 
     case 2:
         print('Sua conversão de R${:.2f} em euro deu €{:.2f}'.format(real,euro))
@@ -15,6 +15,5 @@ match opcao:
     case 3:
         print('Sua conversão de R${:.2f} em kwanza deu {:.2f}Kz'.format(real,kwanza))
 
-
     case _:
-print('opção inválida')
+        print('opção inválida')
