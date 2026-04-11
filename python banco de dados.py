46ea3091baa119572e1c2223f3824489bd627cbb
Raphael Eduardo Ferreira de Oliveira <eduardorafael275@gmail.com>
2026-04-10 22:34:48 -0300

    Conversor de Moedas.py

---
+++ python banco de dados.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+conexao = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",  # coloca sua senha se tiver
+    database="Universidade"
+)
+
+print("Conectado com sucesso!")
