2581c9b7111fa08db7af1f2e9bb4443759521125
Raphael Eduardo Ferreira de Oliveira <eduardorafael275@gmail.com>
2026-04-10 22:53:14 -0300

    Etl.py

--- python banco de dados.py
+++ python banco de dados.py
@@ -1,9 +1,10 @@
 import mysql.connector
+import csv
 
 conexao = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="",  # coloca sua senha se tiver
+    password="",
     database="Universidade"
 )
 
@@ -15,3 +16,8 @@ dados=cursor.fetchall()
 
 for linha in dados:
     print(linha)
+with open("alunos.csv","w", newline="", encoding="utf-8") as arquivo:
+    writer = csv.writer(arquivo)
+
+    colunas = [desc[0]] for desc in cursor.description]
+    writer.writerow(colunas)
